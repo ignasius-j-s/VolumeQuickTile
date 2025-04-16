14801e2a0bd457e58d861b9890e2d8556035ace0
Ignasius <96295999+ignasius-j-s@users.noreply.github.com>
2025-04-16 21:38:46 +0700

    remove unnecessary import

--- app/src/main/java/id/ijs/volumequicktile/VolumeTileService.kt
+++ app/src/main/java/id/ijs/volumequicktile/VolumeTileService.kt
@@ -1,9 +1,6 @@
 package id.ijs.volumequicktile
 
-import android.app.Service
-import android.content.Intent
 import android.media.AudioManager
-import android.os.IBinder
 import android.service.quicksettings.TileService
 
 class VolumeTileService : TileService() {
